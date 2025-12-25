71b92e8180bf724597db49299a1e5fe75985ce97
aastha-git9 <aasthachaturvedi0908@gmail.com>
2025-12-25 15:25:44 +0530

    message

---
+++ Frontend/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("expense-form");
+const expenseList = document.getElementById("expense-list");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const amount = document.getElementById("amount").value;
+  const category = document.getElementById("category").value;
+
+  const li = document.createElement("li");
+  li.textContent = `${title} - ₹${amount} (${category})`;
+
+  expenseList.appendChild(li);
+
+  form.reset();
+});
