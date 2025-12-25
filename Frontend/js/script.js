dd3d8e026c888d7407b986b67459473ca390e13d
aastha-git9 <aasthachaturvedi0908@gmail.com>
2025-12-25 17:11:43 +0530

    Initial expense tracker functionality

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,17 +1,34 @@
-const form = document.getElementById("expense-form");
-const expenseList = document.getElementById("expense-list");
+let balance = 0;
+const balanceAmount = document.getElementById("balance-amount");
+const form = document.getElementById("transaction-form");
+const list = document.getElementById("transaction-list");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const title = document.getElementById("title").value;
+  const text = document.getElementById("text").value;
   const amount = document.getElementById("amount").value;
-  const category = document.getElementById("category").value;
+
+  if (text === "" || amount === "") {
+    alert("Please enter valid details");
+    return;
+  }
+
+  const transactionAmount = Number(amount);
+  balance += transactionAmount;
+
+  balanceAmount.innerText = `₹${balance}`;
 
   const li = document.createElement("li");
-  li.textContent = `${title} - ₹${amount} (${category})`;
+li.innerText = `${text} : ₹${transactionAmount}`;
 
-  expenseList.appendChild(li);
+if (transactionAmount < 0) {
+  li.style.color = "red";
+} else {
+  li.style.color = "green";
+}
+
+list.appendChild(li);
 
   form.reset();
 });
